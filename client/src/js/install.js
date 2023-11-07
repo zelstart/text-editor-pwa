d92af7923ed11839fb14ff0355581cec6ced2705
zelstart <e.pomeroy95@gmail.com>
2023-11-07 17:50:43 -0500

    hide install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,6 +4,7 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 window.deferredPrompt = event;
+butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -16,6 +17,7 @@ butInstall.addEventListener('click', async () => {
 
     promptEvent.prompt();
     window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event

e2087df6f51af95881278526356b473cf493b9db
zelstart <e.pomeroy95@gmail.com>
2023-11-09 16:42:15 -0500

    BUGGY but does save and load text content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,47 +1,50 @@
 import { openDB } from 'idb';
 
+// initialize the database
 const initdb = async () =>
   openDB('jate', 1, {
+    // called if the database version is less than the version provided
     upgrade(db) {
+      // if the object store already exists, do nothing
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // create a new object store with an autoincrementing key
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// add/update content to the database
 export const putDb = async (content) => {
-
   // opens the jate database
-  const jateDb = await openDB('contact', 1);
+  const jateDb = await initdb();
   // create a transaction with readwrite permissions
-  const tx = jateDb.transaction('jate', 'readwrite')
+  const tx = jateDb.transaction('jate', 'readwrite');
   // gets the object store
   const store = tx.objectStore('jate');
-  // adds the text editor content to the object store
-  await store.put(content)
-  // wait for transaction to end
+  // add content to the store with an autoincrementing id
+  await store.put({ id: 1, content: content });
+  // wait for the transaction to complete before closing the database
   await tx.done;
-
+  console.log('Content added to the database');
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// get content from the database
 export const getDb = async () => {
-
-  // opens the database
-  const jateDb = await openDB('contact', 1);
+  // opens the jate database
+  const jateDb = await initdb();
   // create a transaction with readonly permissions
-  const tx = jateDb.transaction('jate', 'readonly')
+  const tx = jateDb.transaction('jate', 'readonly');
   // gets the object store
   const store = tx.objectStore('jate');
-  // retrieve all content from the object store
-  const request = store.getAll();
-  const result = await request;
-  // return the result of the GET
-  return result;
-}
+  // get content from the store with id 1
+  const data = await store.get(1);
+  // wait for the transaction to complete
+  await tx.done;
+  // return the content. if there is no content, return an empty string
+  return data ? data.content : '';
+};
 
 initdb();

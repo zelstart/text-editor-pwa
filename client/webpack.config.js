a6825c3508f5a261a99e2d9fd3ee184e8f8454b6
zelstart <e.pomeroy95@gmail.com>
2023-11-09 17:13:46 -0500

    bigger icons

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
+            sizes: [192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           }
         ]

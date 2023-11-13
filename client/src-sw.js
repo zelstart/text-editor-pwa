dc737157aa97963187bdd7ee84ed82709deb80be
zelstart <e.pomeroy95@gmail.com>
2023-11-12 19:49:41 -0500

    comments

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// asset caching
 registerRoute(
   ({ request }) => ['document', 'manifest', 'script'].includes(request.destination),
   new CacheFirst({

6fcd64bdb5bcfc99c1e4b44103d9538cf54a12fc
zelstart <e.pomeroy95@gmail.com>
2023-11-12 22:23:47 -0500

    added filename to icons

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,6 +40,7 @@ module.exports = () => {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
+            filename: '[name].[ext]'
           }
         ]
       }),
